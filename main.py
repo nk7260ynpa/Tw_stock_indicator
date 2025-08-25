043b6d979baf4c720b7fad6c2f00752c8446036b
Chen <nk7260ynpa@gmail.com>
2025-08-26 00:33:55 +0800

    Add initial implementation of data loading using MyTWSESQLRouter

---
+++ main.py
@@ -0,0 +1,13 @@
+from routers import MyTWSESQLRouter
+
+HOST = "localhost:3306"
+USER = "root"
+PASSWORD = "stock"
+
+router = MyTWSESQLRouter(HOST, USER, PASSWORD)
+
+start_date = "2025-01-01"
+end_date = "2025-06-30"
+security_code = "2330"
+
+data = router.load_data(start_date, end_date, security_code)
